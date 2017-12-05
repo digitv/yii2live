ff3ea8b2a8d9e8d086a6fdb3c17874518ce367bf
Digit <digit.vova@gmail.com>
2017-12-05 12:15:16 +0200

    JS/CSS little changes

--- static/js/yii2live.cmd.js
+++ static/js/yii2live.cmd.js
@@ -110,7 +110,19 @@ yii2liveCmd = function (self) {
             notify: {
                 settings: {
                     showProgressbar: true,
-                    template: '<div class="col-xs-11 col-sm-3 alert alert-{0}" role="alert" data-notify="container"><button type="button" class="close" data-notify="dismiss"><span aria-hidden="true">&times;</span></button><span data-notify="icon"></span><span data-notify="title">{1}</span><span data-notify="message">{2}</span><div class="progress kv-progress-bar" data-notify="progressbar"><div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:100%"></div></div><a href="{3}" data-notify="url" target="{4}"></a></div>',
+                    delay: 5000,
+                    template: '<div class="col-xs-11 col-sm-3 yii2-live-notify alert alert-{0}" role="alert" data-notify="container">' +
+                    '<button type="button" class="close" data-notify="dismiss">' +
+                    '   <span aria-hidden="true">&times;</span>' +
+                    '</button>' +
+                    '<span data-notify="icon"></span>' +
+                    '<span data-notify="title">{1}</span>' +
+                    '<span data-notify="message">{2}</span>' +
+                    '<div class="progress" data-notify="progressbar">' +
+                    '   <div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:100%"></div>' +
+                    '</div>' +
+                    '<a href="{3}" data-notify="url" target="{4}"></a>' +
+                    '</div>'
                 },
                 icons: {
                     success: 'fa fa-check-circle',
